237bba56da776d7eacad4b34af4b96326f3c2d5e
Axisetory <89364852+Axisetory@users.noreply.github.com>
2021-08-22 20:37:16 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var ball;
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,20,20);
+  ball.shapeColor = "green";
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+
+}
+
+
+
+
